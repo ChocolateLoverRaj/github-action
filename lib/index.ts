504a528ec8821bf42e6f858f08bab5ca1ff367fc
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-01 15:59:40 -0500

    Fix echo

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 import { setOutput, getInput } from '@actions/core'
 
-const str = getInput('str')
+const str = getInput('str', { required: true })
 setOutput('str', str.toUpperCase())
+console.log('Input', str, str.length)
+console.log('Set output', str.toUpperCase())
