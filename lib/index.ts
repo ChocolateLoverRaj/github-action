283b5e8a31544853107e7224d94a7b4d748f3d7f
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-10 10:46:56 -0500

    Revert "Log process.env"
    This reverts commit 39fc0faae61f9ef86ae42420c65546b6a856ed96.

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,4 @@
 import { setOutput, getInput } from '@actions/core'
 
-console.log(process.env)
-
 const str = getInput('str', { required: true })
 setOutput('str', str.toUpperCase())
--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,10 @@
-test.todo('works')
+import testGhAction from '../test-lib/testGhAction'
+import { join } from 'path'
+
+test('works', async () => {
+  await expect(testGhAction(join(__dirname, '../dist/index.js'), {
+    str: 'guitar'
+  })).resolves.toEqual({
+    str: 'GUITAR'
+  })
+})
