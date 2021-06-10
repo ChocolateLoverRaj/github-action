39fc0faae61f9ef86ae42420c65546b6a856ed96
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-10 10:42:29 -0500

    Log process.env

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 import { setOutput, getInput } from '@actions/core'
 
+console.log(process.env)
+
 const str = getInput('str', { required: true })
 setOutput('str', str.toUpperCase())
--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,1 @@
-import testGhAction from '../test-lib/testGhAction'
-import { join } from 'path'
-
-test('works', async () => {
-  await expect(testGhAction(join(__dirname, '../dist/index.js'), {
-    str: 'guitar'
-  })).resolves.toEqual({
-    str: 'GUITAR'
-  })
-})
+test.todo('works')
